713aa2b0322c25e251068e23fc7c511631b5f43e
Tan Chu <139019601+tanchu-git@users.noreply.github.com>
2023-09-11 00:23:53 +0200

    Update test_add.py

--- tests/cli/test_add.py
+++ tests/cli/test_add.py
@@ -7,7 +7,6 @@ def test_add(cards_db, cards_cli):
     assert len(all) == 1
     assert all[0] == expected
 
-
 def test_add_with_owner(cards_db, cards_cli):
     cards_cli("add some task -o brian")
     expected = cards.Card("some task", owner="brian", state="todo")
