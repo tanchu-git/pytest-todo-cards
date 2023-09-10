d67a2ab2b3b7eff1e187ace3330a6c274f77292d
Tan Chu <139019601+tanchu-git@users.noreply.github.com>
2023-09-11 00:24:09 +0200

    Update test_errors.py

--- tests/cli/test_errors.py
+++ tests/cli/test_errors.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 @pytest.mark.parametrize("command", ["delete 25",
                                      "start 25",
                                      "finish 25",
@@ -8,7 +7,6 @@ import pytest
 def test_invalid_card_id(cards_db, command, cards_cli):
     out = cards_cli(command)
     assert out == "Error: Invalid card id 25"
-
 
 def test_missing_summary(cards_db, cards_cli):
     out = cards_cli("add")
